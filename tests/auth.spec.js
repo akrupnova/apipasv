e831614bf1f4580d3226ddfc10969e5991260b4a
Anna <anna.krupnovaa@gmail.com>
2021-09-10 20:33:00 -0700

    add wrappers

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -1,47 +1,36 @@
-import supertest from 'supertest';
 import { expect } from 'chai';
+import AuthHelper from '../helpers/auth.helper';
 
 describe('auth', function (){
-    //   const request = supertest(process.env.BASE_URL);
-    let result;
+    let authHelper = new AuthHelper();
 
     describe('successful log in', function() {
         before(async function() {
-            await supertest(process.env.BASE_URL)
-                .post('/auth')
-                .send({ login: process.env.LOGIN, password: process.env.PASSWORD })
-                .then((res) => {
-                    result = res;
-                });
+            await authHelper.get(process.env.LOGIN, process.env.Password);
         });
 
         it('response status code is 200', function() {
-            expect(result.statusCode).to.eq(200);
+            expect(authHelper.response.statusCode).to.eq(200);
 
         });
 
         it('response body contains authorization token', function() {
-            expect(result.body.token).not.to.be.undefined;
+            expect(authHelper.response.body.token).not.to.be.undefined;
         });
     });
 
     describe('log in with wrong credentials should return error', function() {
         before(async function() {
-            await supertest(process.env.BASE_URL)
-                .post('/auth')
-                .send({ login: 'wrong', password: 'wrong' })
-                .then((res) => {
-                    result = res;
-                });
+            await authHelper.get('wrong', 'wrong');
         });
 
         it('response status code is 404', function() {
-            expect(result.statusCode).to.eq(404);
+            expect(authHelper.response.statusCode).to.eq(404);
         });
 
         it('response body contains error message', function() {
 
-                expect(result.body.message).to.eq('Wrong login or password.');
+                expect(authHelper.response.body.message).to.eq('Wrong login or password.');
 
         });
     });
