66991b8c20da6ecc4100db85229ffd559a295af6
Anna <anna.krupnovaa@gmail.com>
2021-09-16 11:03:54 -0700

    add transactions tests

--- helpers/transactions.helper.js
+++ helpers/transactions.helper.js
@@ -1,16 +1,9 @@
 import supertest from 'supertest';
-import UsersHelper from './users.helper';
-import ConfigHelper from '../helpers/config.helper';
-
-let configHelper = new ConfigHelper();
-let userHelper = new UsersHelper();
 
 class TransactionsHelper {
 
     constructor() {
         this.response = null;
-        this.id1 = null;
-        this.id2 = null;
     }
 
     async createTrans(id1, id2, amount) {
@@ -25,17 +18,6 @@ class TransactionsHelper {
             .then((res) => {
                 this.response = res;
             });
-    }
-
-    async createManyTrans(n) {
-        for await (const user of Array(n)) {
-            await userHelper.createUser();
-            this.id1 = userHelper.response.body.id;
-            await userHelper.createUser();
-            this.id2 = userHelper.response.body.id;
-            await configHelper.getConfig();
-            await this.createTrans(this.id1, this.id2, configHelper.amount);
-        }
     }
 
     async getAllTrans() {
